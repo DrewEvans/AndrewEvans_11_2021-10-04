e18f4bb3dd683925fca9f71fe8341622a118a83f
drew <andrew.wr.evans@gmail.com>
2021-10-06 15:18:52 +0200

    update to app & added about page

--- src/App.js
+++ src/App.js
@@ -8,30 +8,6 @@ const Home = lazy(() => import("./pages/Home"));
 const ApartmentPage = lazy(() => import("./pages/ApartmentPage"));
 
 function App() {
-  // const { response, loading, error } = useFetch(
-  //   "http://localhost:5000/api/data",
-  //   {}
-  // );
-  // const apartments = response.data;
-
-  // // if (apartments) {
-  // //   apartments.map((apartment, index) => {
-  // //     console.log(apartment, index);
-  // //   });
-  // // }
-
-  // apartments && !loading
-  //   ? console.log("apartments after load")
-  //   : console.log("no aparts after load");
-  // !error ? console.log(true) : console.log(false);
-
-  // console.log(response);
-  // console.log(loading);
-  // console.log(error);
-  // // const apartments = Array.from(response.data);
-
-  // // response.data.map((x) => console.log(x.id));
-
   return (
     <>
       <Router>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as ApartmentPage } from "./ApartmentPage";
 export { default as ErrorPage } from "./ErrorPage";
+export { default as About } from "./About";
